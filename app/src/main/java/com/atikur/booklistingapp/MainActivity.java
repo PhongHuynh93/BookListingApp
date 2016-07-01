9746eb8820675028f1e4a8a879183a6029f35e75
Atikur Rahman <atik.bd08@gmail.com>
2016-07-01 15:21:59 +0600

    add SearchBooksTask class

--- app/src/main/java/com/atikur/booklistingapp/MainActivity.java
+++ app/src/main/java/com/atikur/booklistingapp/MainActivity.java
@@ -1,13 +1,38 @@
 package com.atikur.booklistingapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityLog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new SearchBooksTask().execute("hello");
+    }
+
+    private class SearchBooksTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            Log.d(TAG, "onPreExecute called.");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.d(TAG, "doing in background.");
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.d(TAG, "onPostExecute called.");
+        }
     }
 }
